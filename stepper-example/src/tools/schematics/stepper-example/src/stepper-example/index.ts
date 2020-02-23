de8f53d626f9684b1e3a05e5f870dab9a77b0053
Farooq Mahmud <farooq@teqniqly.com>
2020-02-22 17:39:23 -0800

    Dasherize file paths.

--- stepper-example/src/tools/schematics/stepper-example/src/stepper-example/index.ts
+++ stepper-example/src/tools/schematics/stepper-example/src/stepper-example/index.ts
@@ -1,13 +1,21 @@
-import { Rule, SchematicContext, Tree, apply, url, mergeWith, MergeStrategy, move } from '@angular-devkit/schematics';
-import { normalize } from '@angular-devkit/core';
+import { Rule, SchematicContext, Tree, apply, url, mergeWith, MergeStrategy, move, template } from '@angular-devkit/schematics';
+import { normalize, strings } from '@angular-devkit/core';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function stepperExample(options: any): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    const normalizedFolderPath = normalize(`${options.path}/${options.name}`);
+    const normalizedFolderPath = normalize(`${strings.dasherize(options.path)}/${strings.dasherize(options.name)}`);
     const files = url('./files');
-    const newTree = apply(files, [move(normalizedFolderPath)]);
+
+    const newTree = apply(
+      files,
+      [move(normalizedFolderPath),
+      template({
+        ...strings,
+        ...options
+      })]);
+
     const rule = mergeWith(newTree, MergeStrategy.Default);
     return rule(tree, context);
   };
