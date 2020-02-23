13cb180828c46c338afff75e8ed43365feb96ad1
Farooq Mahmud <farooq@teqniqly.com>
2020-02-22 16:21:10 -0800

    Stepper component demp

--- stepper-example/src/app/stepper-overview-example/stepper-overview-example.component.ts
+++ stepper-example/src/app/stepper-overview-example/stepper-overview-example.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -16,31 +16,9 @@ export class StepperOverviewExampleComponent implements OnInit {
   nameFormGroup: FormGroup;
   emailFormGroup: FormGroup;
 
-  steps = [
-    {label: 'Confirm your name', content: 'Last name, First name.'},
-    {label: 'Confirm your contact information', content: '123-456-7890'},
-    {label: 'Confirm your address', content: '1600 Amphitheater Pkwy MTV'},
-    {label: 'You are now done', content: 'Finished!'}
-  ];
-
-  /** Returns a FormArray with the name 'formArray'. */
-  get formArray(): AbstractControl | null { return this.formGroup.get('formArray'); }
-
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.formGroup = this.formBuilder.group({
-      formArray: this.formBuilder.array([
-        this.formBuilder.group({
-          firstNameFormCtrl: ['', Validators.required],
-          lastNameFormCtrl: ['', Validators.required],
-        }),
-        this.formBuilder.group({
-          emailFormCtrl: ['', Validators.email]
-        }),
-      ])
-    });
-
     this.nameFormGroup = this.formBuilder.group({
       firstNameCtrl: ['', Validators.required],
       lastNameCtrl: ['', Validators.required],
